a07cb6fac0f7713aa991a311f64c02717e7d7735
lajapathyarun <lajapathy.arun@gmail.com>
2018-03-25 06:15:09 +0530

    lklk

--- TestWebApi.DAL/DbContexts/ProductsContext.cs
+++ TestWebApi.DAL/DbContexts/ProductsContext.cs
@@ -14,6 +14,6 @@ namespace TestWebApi.DAL.DbContexts
             : base("name=ProductsContext")
         {
         }
-        public DbSet<Product> Products { get; set; }
+        public DbSet<Product> Products { get; set; }   //kjlkjljl
     }
 }
