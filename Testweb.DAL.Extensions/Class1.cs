ee752f7211a5016fa9d9ced8cbde10658da615d3
LAJAPATHY ARUN <lajapathy.arun@gmail.com>
2018-03-25 03:08:15 +0200

    DAL Project name changed.

---
+++ Testweb.DAL.Extensions/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testweb.DAL.Extensions
+{
+    public class Class1
+    {
+    }
+}
