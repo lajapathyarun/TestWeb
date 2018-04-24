cb868b2464bc6d501b61fe29166171ce649101e5
LAJAPATHY ARUN <lajapathy.arun@gmail.com>
2018-04-24 05:57:00 +0200

    helo

--- TestWeb.DAL/DbContexts/ProductsContext.cs
+++ TestWeb.DAL/DbContexts/ProductsContext.cs
@@ -14,6 +14,6 @@ namespace TestWebApi.DAL.DbContexts
             : base("name=ProductsContext")
         {
         }
-        public DbSet<Product> Products { get; set; }   //kjlkjljlhhghsdadsa
+        public DbSet<Product> Products { get; set; }
     }
 }
